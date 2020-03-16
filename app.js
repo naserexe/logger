fc598a89000cc93b5f3c636ce823087a8513ef98
Naser <nasersoftcorporation2013@gmail.com>
2020-03-16 20:56:46 +0600

    Custom Error handler added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/error');
 
 // Route files;
 const indexRoute = require('./routes/index');
@@ -14,7 +17,21 @@ const PORT = process.env.PORT || 5000;
 // Logger middleware
 app.use(logger);
 
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
 // Mount Routes
 app.use('/', indexRoute);
+
+// All route will throw an error except those we created
+app.use('*', (req, res) => {
+  res.render('notFound', { error: 'Ops! This Page Could Not Be Found', status: '404' });
+});
+
+
+// Error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
 
-
+// This route will work correctly
 router.get('/', (req, res, next) => {
-  res.json({ msg: 'GET method on root url' });
-  req.reqParams = req.params;
-  next();
+  try {
+    res.render('index', { user: 'Abdullah Naser' });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// This route will throw an error
+router.get('/error', (req, res, next) => {
+  try {
+    // eslint-disable-next-line
+    something_goes_wrong_here___
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
